a267c71392d4d18188cd35ff9a3ea25235846e5b
radionadejda <radionadejda@gmail.com>
2023-10-24 14:54:53 +0300

    add styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import './_App.scss';
 // import './components/Card/_Card.scss';
 // import Heroes from './components/Data/Data.js';
-import {Card} from './components/Card/Card.jsx';
+import { Card } from './components/Card/Card.jsx';
 
 function App() {
     return (
-        <Card/>
+        <div className="body">
+            <h1 className="main title">Массив супергероев</h1>
+            <h2 className="title">Вот такие супергерои у нас есть</h2>
+            <div className="catalogue" id="catalogue">
+                <Card />
+            </div>
+        </div>
     );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,22 @@
-import {Heroes} from '../Data/Data';
-
+// import { Heroes } from "../Data/Data";
 import './_Card.scss';
 
-import {Rating} from './Rating/Rating';
+
+import { Rating } from './Rating/Rating';
 
 export function Card() {
     return (
-        <div className="App">
-            <div>
-                {Heroes[0].name}
+        <div class="hero">
+            <h2 class="title">зеленый велосипед</h2>
+            <div class="description">
+                <div class="text">да</div>
+                <div class="text">красный велосипед</div>
+                <div class="text">roll, baby, roll</div>
+                <div class="text">синий велосипед, серебристый мопед</div>
+                <div class="text">зеленый велосипед настолько же быстр, как его ездок</div>
             </div>
-            <Rating/>
+            <img src="https://shulz.ru/upload/small_1673211680420big.webp" class="image" alt="зеленый велосипед" />
+            <Rating />
         </div>
     );
 }
