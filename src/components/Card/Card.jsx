f58218588b2438a4d366ace7aa60515006c9d1a1
radionadejda <radionadejda@gmail.com>
2023-10-30 09:53:19 +0300

    add card back

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,27 @@
 // import { Heroes } from "../Data/Data";
 import './_Card.scss';
 
-
 import { Rating } from './Rating/Rating';
 
 export function Card(props) {
-    const { name, universe, alterego, occupation, friends, superpowers, url } = props.hero;
-    return (
-        <div className="hero">
-            <h2 className="title">{name}</h2>
-            <div className="description">
-                <div className="text">{universe}</div>
-                <div className="text">{alterego}</div>
-                <div className="text">{occupation}</div>
-                <div className="text">{friends}</div>
-                <div className="text">{superpowers}</div>
-            </div>
-            <img src={url} className="image" alt={name} />
-            <Rating />
-        </div>
-    );
+	const { name, universe, alterego, occupation, friends, superpowers, url, info } = props.hero;
+	return (
+		<div className="card">
+			<div className="hero">
+				<h2 className="title">{name}</h2>
+				<div className="description">
+					<div className="text">{universe}</div>
+					<div className="text">{alterego}</div>
+					<div className="text">{occupation}</div>
+					<div className="text">{friends}</div>
+					<div className="text">{superpowers}</div>
+				</div>
+				<img src={url} className="image" alt={name} />
+				<Rating />
+			</div>
+			<div className="cardBack">
+				<div className="text">{info}</div>
+			</div>
+		</div>
+	);
 }
