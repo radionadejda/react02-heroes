2b9371676c0f77e24f9dd511759600a4c2611fb4
radionadejda <radionadejda@gmail.com>
2023-11-01 13:11:16 +0300

    fix tabs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { heroes } from './components/Data/Data.js';
 import { Card } from './components/Card/Card.jsx';
 
 function App() {
-	return (
-		<div className="body">
-			<div className="catalogue" id="catalogue">
-				{heroes.map((hero, index) => <Card key={index} hero={hero} />)}
-			</div>
-		</div>
-	);
+    return (
+        <div className="body">
+            <div className="catalogue" id="catalogue">
+                {heroes.map((hero, index) => <Card key={index} hero={hero} />)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
